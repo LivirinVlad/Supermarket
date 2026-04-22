d0284090b052995757e4862597d08ac83200ef9f
LivirinVlad <vladlivirin@gmail.com>
2026-04-22 06:34:47 +0300

    b

--- Supermarket.Frontend/src/app/app.routes.ts
+++ Supermarket.Frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
+// import { Routes } from '@angular/router';
+// import { ProductsPage } from './features/products/pages/products-page';
+
+// export const routes: Routes = [{ path: '', redirectTo: 'products', pathMatch: 'full' },
+//   { path: 'products', component: ProductsPage }];
+
 import { Routes } from '@angular/router';
+import { ProductsPage } from './features/products/pages/products-page';
 
 export const routes: Routes = [
-  {
-    path: 'products',
-    loadChildren: () =>
-      import('./features/products/products.routes')
-        .then(m => m.PRODUCT_ROUTES)
-  }
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsPage }
 ];
--- Supermarket.Frontend/src/app/features/products/products.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './pages/product-list/product-list.component';
-
-export const PRODUCT_ROUTES: Routes = [
-  { path: '', component: ProductListComponent }
-];
